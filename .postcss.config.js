601d987cebbd0b4d51e862c9837b9a5e777ee662
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-10-30 11:49:15 +0000

    add cssnano

--- .postcss.config.js
+++ .postcss.config.js
@@ -1,3 +1,13 @@
-module.exports = {
+const postcssConfig = {
   plugins: [require("postcss-preset-env"), require("autoprefixer")],
 };
+
+if (process.env.NODE_ENV === "production") {
+  postcssConfig.plugins.push(
+    require("cssnano")({
+      preset: "default",
+    })
+  );
+}
+
+module.exports = postcssConfig;
