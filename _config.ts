9dc848adb7572f574b45567eaaf5c850c17950c7
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-03-05 17:42:35 +0900

    replace astro to deno

---
+++ _config.ts
@@ -0,0 +1,7 @@
+import lume from "lume/mod.ts";
+
+const site = lume({
+  dest: "./dist",
+});
+
+export default site;
---
+++ site.ts
@@ -0,0 +1,4 @@
+import lume from "lume/mod.ts";
+
+const site = lume();
+export default site;
