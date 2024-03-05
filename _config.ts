4fe69b1e55d6f22ae3551e4e6772c00c1b46d8dc
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-03-05 18:25:23 +0900

    astro -> lume

--- _config.ts
+++ _config.ts
@@ -1,7 +1,14 @@
 import lume from "lume/mod.ts";
+import lightningCss from "lume/plugins/lightningcss.ts";
+import favicon from "lume/plugins/favicon.ts";
 
 const site = lume({
   dest: "./dist",
-});
+})
+  .use(lightningCss())
+  .use(favicon({
+    input: "./favicon.png",
+  }))
+  .ignore("README.md", "LICENSE");
 
 export default site;
