f38bde4009e81006c5989bcb8b059991b5e8d6ec
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2026-02-01 19:54:32 +0000

    ci: consolidate all checks into nix flake check
    Move typos and gitleaks checks into flake.nix, simplifying CI to a
    single nix flake check command that runs all validations:
    - PDF page count (must be 2 pages)
    - Spell checking with typos
    - Secret detection with gitleaks

--- flake.nix
+++ flake.nix
@@ -40,6 +40,28 @@
               echo "PDF has 2 pages"
               touch $out
             '';
+
+          typos = pkgs.runCommand "check-typos"
+            {
+              nativeBuildInputs = [ pkgs.typos ];
+              src = ./.;
+            }
+            ''
+              cd $src
+              typos
+              touch $out
+            '';
+
+          gitleaks = pkgs.runCommand "check-gitleaks"
+            {
+              nativeBuildInputs = [ pkgs.gitleaks ];
+              src = ./.;
+            }
+            ''
+              cd $src
+              gitleaks detect --source . --config .gitleaks.toml --no-git
+              touch $out
+            '';
         }
       );
 
